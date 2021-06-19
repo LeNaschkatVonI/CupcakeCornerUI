54d775440a923fd2bd5f6e6bdff0b2c2632cee90
Maxim Vainikka <rithmique@yahoo.com>
2021-06-19 12:41:21 +0300

    Initial Commit

---
+++ CupcakeCornerUI/CupcakeCornerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerUIApp.swift
+//  CupcakeCornerUI
+//
+//  Created by Максим Нуждин on 19.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
